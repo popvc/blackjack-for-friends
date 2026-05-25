54b9a0733da55029df151874cb579740936df3c9
popovc <popovc@protonmail.com>
2026-05-25 15:14:03 -0400

    backend tsconfig set

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+// src/index.ts
+import express from "express";
+import type { Request, Response } from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
