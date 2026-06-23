d365d6ea41cabd4729bdc30dcb383f87467db1a8
popovc <popovc@protonmail.com>
2026-06-23 16:24:34 -0400

    Bun init defaults, axios, Zustand

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import { APITester } from "./APITester";
+import "./index.css";
+
+import logo from "./logo.svg";
+import reactLogo from "./react.svg";
+
+export function App() {
+  return (
+    <div className="app">
+      <h1>Bun + React</h1>
+      <p>
+        Edit <code>src/App.tsx</code> and save to test HMR
+      </p>
+      {/*<APITester />*/}
+    </div>
+  );
+}
+
+export default App;
