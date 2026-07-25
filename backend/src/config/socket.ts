8bbb15e9fee1cdd0db023920ad87938c625540ea
popovc <popovc@protonmail.com>
2026-07-25 13:16:46 -0400

    SocketIO auth middleware

--- backend/src/config/socket.ts
+++ backend/src/config/socket.ts
@@ -1,12 +1,11 @@
 import { Server as Engine } from "@socket.io/bun-engine";
 import { Server } from "socket.io";
-import express from "express";
 import type { Request, Response } from "express";
 import { CORS } from "./cors";
+import type { AuthUser } from "./authToken";
 
 interface SocketData {
-  recipientId: string;
-  senderId: string;
+  user: AuthUser;
 }
 
 //SocketIO works, but is not ideal for this. Considering swapping it out for native Bun Sockets
@@ -17,21 +16,22 @@ interface SocketData {
 //heartbeat
 //maxpayload
 
-const app = express();
+//const app = express();
+function startSocketServer() {
+  const io = new Server<SocketData>({
+    cors: CORS,
+  });
 
-const io = new Server({
-  cors: CORS,
-});
+  const engine = new Engine({
+    path: "/socket.io/",
+  });
 
-const engine = new Engine({
-  path: "/socket.io/",
-});
+  io.bind(engine);
 
-io.bind(engine);
+  //io.use()
 
-//io.use()
-
-io.on("connection", (socket) => {});
-//Needs auth middleware passed
+  io.on("connection", (socket) => {});
+  //Needs auth middleware passed
+}
 
 //needs socket map for connected users
--- backend/src/middleware/socketAuth.middleware.ts
+++ backend/src/middleware/socketAuth.middleware.ts
@@ -1,12 +1,37 @@
-import type { Socket } from "@socket.io/bun-engine/dist/socket";
+//import type { Socket } from "@socket.io/bun-engine/dist/socket";
 import type { NextFunction } from "express";
+import { verifyToken } from "../config/authToken";
+import { Socket } from "socket.io";
 
 export const socketAuthMiddleware = async (socket: Socket, next: NextFunction) => {
-    try {
+  try {
+    const token = socket.handshake.headers.cookie
+      ?.split("; ")
+      .find((row) => row.startsWith("jwt="))
+      ?.split("=")[1];
 
-        next();
-    }catch (e: unknown){
-        console.log("SocketIO auth error:", e)
-        next(new Error("Unauthorized"))
+    console.log("socketAuthMiddleware", token);
+
+    if (!token) {
+      console.log("Socket connection rejected: Auth token not found");
+      return next(new Error("Unauthorized - Token not found"));
     }
+
+    const user = await verifyToken(token);
+
+    if (!user) {
+      console.log("Socket connection rejected: Invalid token");
+      return next(new Error("Unauthorized - Invalid token"));
+    }
+
+    //more extensions
+    socket.data.user = user;
+
+    console.log(`SocketIO authenticated. ${user}`)
+
+    next();
+  } catch (e: unknown) {
+    console.log("SocketIO auth error:", e);
+    next(new Error("Unauthorized - Authentication failed"));
+  }
 };
