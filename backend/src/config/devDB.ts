059f2d7a66a28a4e7672ebbcc6b4ab3da0bf71de
popovc <popovc@protonmail.com>
2026-08-02 14:27:39 -0400

    default users for tester

--- backend/src/config/devDB.ts
+++ backend/src/config/devDB.ts
@@ -1,9 +1,45 @@
+import bcrypt from "bcryptjs";
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import Profile from "../models/profile.schema";
+
+//for testing purposes only
+//testuser1@example.com
+//testuser2@example.com
+//testuser3@example.com
+//testuser4@example.com
+//1234567890123456
+
+let _n = 0;
+
+async function hashPassword(password: string): Promise<string> {
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  return hashedPassword;
+}
+
+async function createTestProfile() {
+  const n = ++_n;
+
+  const newProfile = new Profile({
+    userId: `1234567890123456789${n}`,
+    username: `testuser${n}`,
+    email: `testuser${n}@example.com`,
+    password: await hashPassword("1234567890123456"),
+  });
+
+  await newProfile.save();
+}
 
 export const connectDevDB = async () => {
+  process.on("exit", () => {
+    mongod.stop();
+  });
+
   const mongod = await MongoMemoryServer.create();
-  const uri = mongod.getUri();
+
+  const uri = `${mongod.getUri()}?retryWrites=false`;
+  console.log("mongoduri", uri);
 
   if (!mongod) {
     console.error("failed to start test DB");
@@ -12,6 +48,11 @@ export const connectDevDB = async () => {
 
   try {
     const pool = await mongoose.connect(uri);
+
+    for (let i = 0; i < 5; i++) {
+      await createTestProfile();
+    }
+
     console.log(
       `In-memory MongoDB connected to '${pool.connection.name}' at '${pool.connection.host}'`,
     );
@@ -20,7 +61,4 @@ export const connectDevDB = async () => {
     process.exit(1);
   }
 
-  process.on("exit", () => {
-    mongod.stop();
-  });
 };
